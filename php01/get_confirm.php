f4dd5dab093decd63cd4aeffb6ce6022255dd856
momo <s1318258@cst.otsuma.ac.jp>
2020-05-15 15:00:23 +0900

    初回登録

---
+++ php01/get_confirm.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_GET["name"];
+$mail = $_get["mail"];
+
+
+
+
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>GET練習（受信）</title>
+</head>
+<body>
+お名前：
+Mail：
+<ul>
+<li><a href="index.php">index.php</a></li>
+</ul>
+</body>
+</html>
